056a47c8b17717c4365d8085e237238867d0a525
luztez <emelsi15@gmail.com>
2015-10-21 14:22:04 +0000

    making the login work and pulling in the api

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,21 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
-
+				<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+				<script>
+				  $.ajax({
+				  url:"https://www.kimonolabs.com/api/9j7lotp6?apikey=GuzWad3GJVgnt2TfuJMJmeANHfL7BOj0",
+				  crossDomain: true,
+				  dataType: "jsonp",
+				  success: function (response) {
+				    console.log("it worked")
+						console.log(response)
+				  },
+				  error: function (xhr, status) {
+				    console.log("it didn't worked")
+				  }
+				});</script>
 				<div class="panel-body">
-					You are logged in!
 				</div>
 			</div>
 		</div>
