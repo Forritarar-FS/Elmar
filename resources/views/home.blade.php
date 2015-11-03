321fed40a34d0976cce4368fe56b646c83d1834e
luztez <emelsi15@gmail.com>
2015-11-03 15:29:43 +0000

    printing the data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+	<div id="data"></div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
@@ -13,13 +14,15 @@
 				success: function (response) {
 					console.log("it worked")
 					console.log(response)
+					$("#data").html(response.results.collection1[4].team1.text);
+
 				},
 				error: function (xhr, status) {
 					console.log("it didn't worked")
 				}
-				var obj = jQuery.parseJSON( '{ "name": "John" }' );
-				alert( obj.name === "John" );
-			});</script>
+			});
+
+			</script>
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
 				<div class="panel-body">
