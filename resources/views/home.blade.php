5d3765b1eb23211dc32c4319e7034d18f1081dae
luztez <emelsi15@gmail.com>
2015-11-05 16:08:26 +0000

    created a loop for all teams

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,21 @@
 
 @section('content')
 <div class="container">
-	<div id="data"></div>
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 			<script>
 				$.ajax({
-				url:"https://www.kimonolabs.com/api/9j7lotp6?apikey=GuzWad3GJVgnt2TfuJMJmeANHfL7BOj0",
+				url:"https://www.kimonolabs.com/api/5f30ts1o?apikey=GuzWad3GJVgnt2TfuJMJmeANHfL7BOj0",
 				crossDomain: true,
 				dataType: "jsonp",
 				success: function (response) {
 					console.log("it worked")
 					console.log(response)
-					$("#data").html(response.results.collection1[4].team1.text);
+					var coll = response.results.collection1;
+					for (var i = 0; i < coll.length; i++) {
+						$("#allData").append("<div class='panel panel-default'><div class='panel-heading'><div class='panel-body'>" + coll[i].team1.text + " " + coll[i].team1score + " - " + coll[i].team2score + " " + coll[i].team2.text + "<br></div> </div></div>");
+					}
 
 				},
 				error: function (xhr, status) {
@@ -23,11 +25,7 @@
 			});
 
 			</script>
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-				<div class="panel-body">
-				</div>
-			</div>
+					<h2 id="allData" align="center"></h2>
 		</div>
 	</div>
 </div>
